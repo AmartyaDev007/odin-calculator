2e3f8e467674c94eba0b6d75332771fc52eeb8f7
AmartyaDev007 <162921312+AmartyaDev007@users.noreply.github.com>
2025-07-24 14:47:43 +0530

    add functions related to add multiply etc

---
+++ code.js
@@ -0,0 +1,52 @@
+let operandOne = 0;
+let operandTwo = 0;
+
+let mainDisplay = 0;
+
+// this variable displays the top smaller greyed out text in the display
+let altDisplay;
+
+
+const allButtons = document.querySelector(".all-btn-box");
+
+allButtons.addEventListener('click', event => {
+    let target = event.target;
+
+    if (target.classList.contains('number')) {
+        event.target.textContent
+    }
+
+    else if (target.classList.contains('operator')) {
+
+    }
+    switch(target.id) {
+        case 'ac-button':
+            break;
+        
+        case 'backspace-button':
+            break;
+
+        case 'decimal-button':
+            break;
+
+        case 'equal-button':
+            break;
+    }
+
+    function add (a, b) { 
+        return a + b;
+    }
+
+function subtract (a, b) { 
+    return a - b;
+}
+
+function multiply (a, b) { 
+    return a * b;
+}
+
+function divide (a, b) { 
+    return a / b;
+}
+
+
